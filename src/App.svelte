063c1d43b74ff27b047a4f288f78ceefbf9abd05
Jasper Travers <wtravers@ucsd.edu>
2019-11-12 21:27:06 -0800

    renamed Pair -> Group

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,19 @@
 <script>
   import { onMount } from 'svelte';
-  import Pair from './Pair.svelte';
+  import Group from './Group.svelte';
   import sketches from './sketches.js';
 
-	let name = 'Jasper';
+	let name = 'you';
 </script>
 
 <div>
-  <h1>Hello {name}!</h1>
+  <h1>Hey {name}</h1>
   <div>
-    <Pair sketch="{sketches.s1}"/>
-    <Pair sketch="{sketches.s2}"/>
-    <Pair sketch="{sketches.s3}"/>
-    <Pair sketch="{sketches.sand}"/>
-    <Pair sketch="{sketches.zip}"/>
-    <Pair sketch="{sketches.linspace}"/>
+    <Group sketch="{sketches.s1}"/>
+    <Group sketch="{sketches.s2}"/>
+    <Group sketch="{sketches.s3}"/>
+    <Group sketch="{sketches.sand}"/>
+    <Group sketch="{sketches.zip}"/>
+    <Group sketch="{sketches.linspace}"/>
   </div>
 </div>
