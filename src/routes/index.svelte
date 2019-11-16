4f7632da58660c8ecf31b655d38dcef8c9e8d7d5
asweingarten <asweingarten@gmail.com>
2019-11-16 11:52:45 -0800

    refactor runtime
    - convert jaspertravers/codeshop into a sapper project
    - $runtime stores function objects instead of function source code

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onMount } from 'svelte';
+  import P5Pair from '../components/P5/Pair.svelte';
+
+  import {runtime} from '../stores/runtime';
+
+</script>
+
+<svelte:head>
+  <title>Where Chunking Happens</title>
+</svelte:head>
+
+<div id="runtime">
+  {#each Object.keys($runtime) as x}
+    <span>{x}</span>
+  {/each}
+</div>
+<div>
+  {#each Object.values($runtime) as func}
+    <P5Pair sketch="{func}"/>
+  {/each}
+</div>
